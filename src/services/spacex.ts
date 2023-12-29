e7f12c2bcf3118b5c3e27543ffad732bcb7e95f5
Mateo Bellini <bellinimateo872@gmail.com>
2023-12-29 15:07:06 +0100

    Proyecto finalizado

---
+++ src/services/spacex.ts
@@ -0,0 +1,28 @@
+import { type APISpaceXResponse, type Doc } from "../types/api";
+
+export const getLatestLaunches = async () => {
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: "asc",
+        },
+        limit: 12,
+      },
+    }),
+  });
+  const { docs: launches }: APISpaceXResponse = await res.json();
+  return launches;
+};
+
+export const getLaunchBy = async ({ id }: { id: string }) => {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+  const launch: Doc = await res.json();
+
+  return launch;
+};
