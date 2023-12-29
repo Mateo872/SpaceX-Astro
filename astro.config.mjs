c6b998a2c6d20ca7d34226e2969a11d240adb2c2
Mateo Bellini <bellinimateo872@gmail.com>
2023-12-29 16:40:10 +0100

    Configuración de Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel/serverless";
 import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  output: "hybrid",
+  output: "server",
+  adapter: vercel()
 });
